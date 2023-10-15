2495c694407439e478033c935642cea38974cc23
pikle2x2 <74595155+pikle2x2@users.noreply.github.com>
2023-10-15 19:38:02 +0200

    Update symulacja.py

--- symulacja.py
+++ symulacja.py
@@ -3,6 +3,7 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
 import time
+import math
 
 # Example function to simulate reading analog data from the sensor
 def get_analog_data():
@@ -12,36 +13,12 @@ def get_analog_data():
         return random.randint(60, 100)  # Simulated heart rate data
     else:
         beat_counter += 1
-        return random.randint(90, 120)  # Simulated heart rate data with higher current
+        time_since_beat = (time.time() - beat_start_time) % (1 / 60)
+        return random.randint(90, 120) + math.sin(2 * math.pi * 60 * time_since_beat) * 10  # Simulated heart rate data with 60 Hz oscillation
 
 # Example function to convert analog data to BPM
 def convert_to_bpm(analog_data):
     return analog_data
-
-# ...
-# Rest of the code remains the same
-
-# Initialize data
-x_data = []
-y_data = []
-start_time = time.time()
-beat_counter = 0
-
-# Function to update BPM display
-def update_bpm_display():
-    analog_data = get_analog_data()
-    bpm = convert_to_bpm(analog_data)
-    bpm_label.config(text=f"Current BPM: {bpm}")
-
-    # Update graph
-    x_data.append(time.time() - start_time)
-    y_data.append(analog_data)
-    ax.clear()
-    ax.plot(x_data, y_data)
-    canvas.draw()
-
-    # Schedule the function to run after 1000ms (1 second)
-    root.after(1000, update_bpm_display)
 
 # Create main window
 root = tk.Tk()
@@ -75,6 +52,24 @@ canvas.get_tk_widget().pack()
 x_data = []
 y_data = []
 start_time = time.time()
+beat_counter = 0
+beat_start_time = start_time
+
+# Function to update BPM display
+def update_bpm_display():
+    analog_data = get_analog_data()
+    bpm = convert_to_bpm(analog_data)
+    bpm_label.config(text=f"Current BPM: {bpm}")
+
+    # Update graph
+    x_data.append(time.time() - start_time)
+    y_data.append(analog_data)
+    ax.clear()
+    ax.plot(x_data, y_data)
+    canvas.draw()
+
+    # Schedule the function to run after 1000ms (1 second)
+    root.after(1000, update_bpm_display)
 
 # Start the data update loop
 update_bpm_display()
