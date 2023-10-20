ea60fc0f22daf7e60ce68d29ce2d436ca2246c36
pikle2x2 <74595155+pikle2x2@users.noreply.github.com>
2023-10-20 13:55:24 +0200

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,31 +1,34 @@
 
 import spidev
-import RPi.GPIO as GPIO
+import time
 
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
-BUTTON_PIN = 17
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# MCP3008 setup
+# Define SPI bus and device (Chip Select)
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
+# Define function to read ADC channel
 def read_adc(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
+# Define function to convert ADC value to voltage
+def adc_to_voltage(adc_value):
+    return (adc_value / 1023.0) * 3.3
+
 try:
     while True:
-        x_value = read_adc(0)
-        y_value = read_adc(1)
-        button_state = GPIO.input(BUTTON_PIN)
-
-        print(f'X Value: {x_value}, Y Value: {y_value}, Button State: {"Pressed" if not button_state else "Released"}')
-
-        if not button_state:
-            print("Button Pressed!")
+        # Read ADC value from channel 0
+        adc_value = read_adc(0)
+        
+        # Convert ADC value to voltage
+        voltage = adc_to_voltage(adc_value)
+        
+        # Print voltage
+        print(f"Voltage: {voltage:.2f} V")
+        
+        # Wait for a short time (optional)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    spi.close()
